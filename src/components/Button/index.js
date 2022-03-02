be5c0bc0ef9296de835d7796cc89ba2d97c5ae55
Humberto Ham <hamduranhumberto@gmail.com>
2022-03-02 15:36:29 -0600

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Styles
+import { Wrapper } from './Button.styles';
+
+const Button = ({ text, callback }) => (
+
+    <Wrapper type="button" onClick={callback}>
+        {text}
+    </Wrapper>
+
+
+
+
+);
+
+export default Button;
---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import RMBDLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <Link to='/'>
+            <LogoImg src={RMBDLogo} alt= 'rmdb-logo'/>
+            </Link>
+            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo'/>
+
+
+        </Content>
+
+    </Wrapper>
+);
+
+export default Header;
+
+
+
+
+
+
+
